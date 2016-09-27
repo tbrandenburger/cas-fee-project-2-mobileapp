6c0f6b70f2e5baea0647d905595d0f3e33bc099c
Thomas Brandenburger <mail@thomasbrandenburger.ch>
2016-09-27 12:44:46 +0200

    karma added for testing

---
+++ app/pages/accounts/accounts.service.spec.ts
@@ -0,0 +1,32 @@
+import {beforeEachProviders, it, describe, expect, inject} from '@angular/core/testing';
+import {AccountsService} from './accounts.service';
+
+describe('Accounts Service', () => {
+
+    beforeEachProviders(() => [AccountsService]);
+
+    it('should return a non empty array', inject([AccountsService], (accountService) => {
+
+            let result = accountService.getAnswers();
+
+            expect(Array.isArray(result)).toBeTruthy;
+            expect(result.length).toBeGreaterThan(0);
+        }
+    ));
+
+    it('should return one random answer as a string', inject([AccountsService], (accountService) => {
+            expect(typeof accountService.getRandomAnswer()).toBe('string');
+        }
+    ));
+
+    it('should have both yes and no available in result set', inject([AccountsService], (accountService) => {
+
+            let result = accountService.getAnswers();
+
+            expect(result).toContain('Yes');
+            expect(result).toContain('No');
+
+        }
+    ));
+
+});
